010328c2d6c51b1a61433c7ebb5bacd4ae949667
James Shkolnik <james.shkolnik@zenpayroll.com>
2016-02-20 13:02:46 -0800

    Only load middleware in development and test envs

--- lib/rack_webpack/railtie.rb
+++ lib/rack_webpack/railtie.rb
@@ -1,7 +1,9 @@
 module RackWebpack
   class Railtie < Rails::Railtie
     initializer "rack_webpack.insert_middleware" do |app|
-      app.config.middleware.use "RackWebpack::Middleware"
+      if Rails.env.in?(%w(development test))
+        app.config.middleware.use "RackWebpack::Middleware"
+      end
     end
   end
 end
