201948d119afefafdf2ea7a0ee334b8089489249
James Shkolnik <james.shkolnik@zenpayroll.com>
2016-02-23 22:06:53 -0800

    Load middleware at the top of the stack

--- lib/rack_webpack/hooks.rb
+++ lib/rack_webpack/hooks.rb
@@ -1,12 +1,11 @@
 require 'rack/server'
 
+
 class Rack::Server
   def start_with_webpack
     RackWebpack::WebpackRunner.run
     start_without_webpack
-
   end
-
   alias_method_chain :start, :webpack
 end
 
--- lib/rack_webpack/railtie.rb
+++ lib/rack_webpack/railtie.rb
@@ -2,7 +2,7 @@ module RackWebpack
   class Railtie < Rails::Railtie
     initializer "rack_webpack.insert_middleware" do |app|
       if Rails.env.in?(%w(development test))
-        app.config.middleware.use "RackWebpack::Middleware"
+        app.config.middleware.insert_before 0, 'RackWebpack::Middleware'
       end
     end
   end
