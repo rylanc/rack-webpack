4e9ce4f1a569a00fe272cbf3bda3e307f1717ce2
Nikhil Mathew <nikhilmat@gmail.com>
2016-03-02 23:33:54 -0800

    Ensure File Exists

---
+++ spec/rack-webpack/file_mutex_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe RackWebpack::FileMutex do
+  describe '._default_locks_dir' do
+    subject { RackWebpack::FileMutex._default_locks_dir }
+
+    it { is_expected.to be_a(Pathname) }
+  end
+
+  describe '.new' do
+    subject { RackWebpack::FileMutex.new('webpack-test') }
+
+    it 'creates the directory if it does not exist' do
+      expect(FileUtils).to receive(:mkdir_p).with(RackWebpack::FileMutex.locks_dir.parent) { true }
+      subject
+    end
+  end
+end
