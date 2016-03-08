f3a4989aaa8938ae6866568e28e3ceb33a19f24b
Nikhil Mathew <nikhilmat@gmail.com>
2016-03-08 11:20:44 -0800

    Disable railtie

--- lib/rack_webpack/railtie.rb
+++ lib/rack_webpack/railtie.rb
@@ -1,14 +1,14 @@
 module RackWebpack
   class Railtie < Rails::Railtie
     initializer "rack_webpack.insert_middleware" do |app|
-      if Rails.env.in?(%w(development test))
-        proxy = ENV['RACK_WEBPACK_PROXY'] 
-        
+      if Rails.env.in?(%w(development test)) && !RackWebpack.config.disable
+        proxy = ENV['RACK_WEBPACK_PROXY']
+
         if proxy.blank?
           default_proxy = RackWebpack.curb_available ? 'Curb' : 'NetHttp'
           proxy ||= "RackWebpack::Middleware::#{default_proxy}Proxy"
         end
-        
+
         app.config.middleware.insert_before 0, proxy
       end
     end
