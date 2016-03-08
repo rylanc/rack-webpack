e965a6d8b4ed1a7ab5adc75d2df79f7f062b9c72
Nikhil Mathew <nikhilmat@gmail.com>
2016-03-08 13:27:08 -0800

    Fix load order

--- lib/rack-webpack.rb
+++ lib/rack-webpack.rb
@@ -13,7 +13,6 @@ module RackWebpack
 
     def configure
       yield(config)
-      require 'rack_webpack/railtie' if defined?(Rails)
     end
   end
 end
@@ -32,3 +31,4 @@ require 'rack_webpack/socket_http'
 require 'rack_webpack/webpack_runner'
 require 'rack_webpack/middleware'
 require 'rack_webpack/hooks'
+require 'rack_webpack/railtie' if defined?(Rails)
--- lib/rack_webpack/railtie.rb
+++ lib/rack_webpack/railtie.rb
@@ -1,6 +1,6 @@
 module RackWebpack
   class Railtie < Rails::Railtie
-    initializer :rack_webpack do |app|
+    initializer :rack_webpack, after: :load_config_initializers do |app|
       if Rails.env.in?(%w(development test)) && !RackWebpack.config.disable
         proxy = ENV['RACK_WEBPACK_PROXY']
 
