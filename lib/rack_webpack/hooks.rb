751551bbb3a3394ce9a6f68d1252ba27600fe58c
Nikhil Mathew <nikhilmat@gmail.com>
2016-03-07 23:08:15 -0800

    Add disable option

--- lib/rack_webpack/hooks.rb
+++ lib/rack_webpack/hooks.rb
@@ -3,12 +3,12 @@ require 'rack/server'
 
 class Rack::Server
   def start_with_webpack
-    RackWebpack::WebpackRunner.run
+    RackWebpack::WebpackRunner.run unless RackWebpack.config.disable
     start_without_webpack
   end
   alias_method_chain :start, :webpack
 end
 
 at_exit do
-  RackWebpack::WebpackRunner.exit
+  RackWebpack::WebpackRunner.exit unless RackWebpack.config.disable
 end
