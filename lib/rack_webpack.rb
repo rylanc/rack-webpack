2041b6037f5812736242ad57804cfbd7018ab3f0
James Shkolnik <james.shkolnik@zenpayroll.com>
2016-03-15 15:59:00 -0700

    Rename rack-webpack.rb to rack_webpack.rb to fit standards

---
+++ lib/rack_webpack.rb
@@ -0,0 +1,39 @@
+module RackWebpack
+  Configuration = Struct.new(
+    :host,
+    :port,
+    :webpack_server_options,
+    :proxy,
+    :asset_regex,
+    :disable_runner
+  )
+
+  class << self
+    attr_accessor :curb_available
+
+    def config
+      # Host and Port defaults
+      @config ||= Configuration.new('localhost', '8080')
+    end
+
+    def configure
+      yield(config)
+    end
+  end
+end
+
+begin
+  require 'curl'
+  RackWebpack.curb_available = Curl.const_defined?('CURLOPT_UNIX_SOCKET_PATH')
+rescue LoadError
+  RackWebpack.curb_available = false
+end
+
+require 'rack_webpack/version'
+require 'rack_webpack/file_mutex'
+require 'rack_webpack/socket_http'
+
+require 'rack_webpack/webpack_runner'
+require 'rack_webpack/middleware'
+require 'rack_webpack/hooks'
+require 'rack_webpack/railtie' if defined?(Rails)
